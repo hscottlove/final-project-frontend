7f2fc2254b1bd7e90ee48fbd9d2e588fbc7b4a03
Scottlove <haleyscottlove@gmail.com>
2019-09-05 11:21:54 -0700

    added profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 class Profile extends Component {
 render() {
     return (
-        <h1>Profile</h1>
+       <>
+            <section class="video-header">
+                <div class="row">
+                    <div class="col-md-12 text-center mt-4">
+                        <h1 class="display-4">Profile</h1>
+                        <p class="lead">Welcome to the team!</p>
+                        <hr class="divider" />
+                    </div>
+                </div>
+            </section>
+
+            <section class="video-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <img src="./images/profile-img.png" alt="profile-img" width="200px" class="img-thumbnail" />
+                            <h3>Scott L.</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+       </>
     )
 }
 
