f6fb1dabcb7d6f138f912f687bfaedf4c329d69b
Scottlove <haleyscottlove@gmail.com>
2019-09-06 13:54:40 -0700

    added className to footer

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import Login from '../components/Auth/Login';
 import Register from '../components/Auth/Register';
@@ -9,13 +9,38 @@ import Profile from '../components/Profile/Profile';
 
 
 
-export default (
-    <Switch>
-        <Route exact path="/" component={ Home }/>
-        <Route path="/login" component={ Login }/>
-        <Route path="/register" component={ Register }/>
-        <Route path="/feed" component={ Feed }/>
-        <Route path="/videos" component={ Videos }/>
-        <Route path="/profile" component={ Profile }/>
-    </Switch>
-)
+// export default (
+//     <Switch>
+//         <Route exact path="/" component={ Home }/>
+//         <Route path="/login" component={ Login }/>
+//         <Route path="/register" component={ Register }/>
+//         <Route path="/feed" component={ Feed }/>
+//         <Route path="/videos" component={ Videos }/>
+//         <Route path="/profile" component={ Profile }/>
+//     </Switch>
+// )
+
+
+export default withRouter(({ setCurrentUser, currentUser, history }) => {
+    const PrivateRoute = ({component: Component, ...rest}) => (
+      <Route {...rest} render={(props) => (
+        currentUser
+          ? <Component {...props} />
+          : <Redirect to='/login' />
+      )} />
+    );
+  
+    return (
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' render={() => <Login history={history} setCurrentUser={setCurrentUser} />} />
+        <Route path='/register' component={Register} />
+        <PrivateRoute path='/feed' component={Feed} />
+        <PrivateRoute path='/videos' component={Videos} />
+        <PrivateRoute path='/profile' component={Profile} />
+      </Switch>
+    );
+  });
+
+
+
