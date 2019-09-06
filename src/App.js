91346005a39de224fdde57563f23820d8221c754
Scottlove <haleyscottlove@gmail.com>
2019-09-06 13:46:04 -0700

    updated routes app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import MyRoutes from './config/Routes';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import Routes from './config/Routes';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import { API_URL } from './constants';
 import './App.css';
 
 class App extends Component {
@@ -9,16 +12,31 @@ class App extends Component {
     currentUser: localStorage.getItem('uid'),
   };
 
+  setCurrentUser = (userId) => {
+    this.setState({ currentUser: userId });
+    localStorage.setItem('uid', userId);
+  };
+
+  logout = () => {
+    localStorage.removeItem('uid');
+    axios.post(`${API_URL}/auth/logout`, { withCredentials: true })
+      .then(res => {
+        console.log(res);
+        this.setState({ currentUser: null });
+        this.props.history.push('/login');
+      })
+      .catch(err => console.log(err));
+  };
 
   render() {
   return (
   <>
    <Navbar />
-   { MyRoutes }
+   { Routes }
    <Footer />
   </>
   );
   }
 }
 
-export default App;
+export default withRouter(App);
