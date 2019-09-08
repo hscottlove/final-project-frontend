0ecbcd440a16a28ec63f60e206df8a3ce9450f9c
Scottlove <haleyscottlove@gmail.com>
2019-09-07 19:45:10 -0700

    added profile class

--- src/components/containers/ProfileContainer.js
+++ src/components/containers/ProfileContainer.js
@@ -2,3 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Profile from '../components/Profile/Profile';
 import { API_URL } from '../constants';
+
+class ProfileContainer extends Component {
+    render() {
+        return(
+            <h1>profileconatiner</h1>
+        )
+    }
+};
+  
+  export default ProfileContainer;
